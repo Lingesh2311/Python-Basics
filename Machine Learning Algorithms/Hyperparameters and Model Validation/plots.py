deeeb44544c82830084ce3052eeaf031d0be612c
Lingesh K <lingesh.k.2311@gmail.com>
2019-06-30 02:25:19 +0100

    Add plots

---
+++ Machine Learning Algorithms/Hyperparameters and Model Validation/plots.py
@@ -0,0 +1,17 @@
+import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import make_pipeline
+
+def PolynomialRegression(degree=2, **kwargs):
+    return make_pipeline(PolynomialFeatures(degree),
+                         LinearRegression(**kwargs))
+
+def make_data(N=30, err=0.8, rseed=1):
+    # randomly sample the data
+    rng = np.random.RandomState(rseed)
+    X = rng.rand(N, 1) ** 2
+    y = 10 - 1. / (X.ravel() + 0.1)
+    if err > 0:
+        y += err * rng.randn(N)
+    return X, y
